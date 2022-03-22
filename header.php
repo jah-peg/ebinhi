fcfad1543db947817efb54a559c80f4a852573dc
Elijah Santos <65294557+jah-peg@users.noreply.github.com>
2022-03-22 22:17:02 +0800

    added files

---
+++ header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>E-binhi</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+	<style type="text/css">
+		<?php
+			include 'css/style.css';
+		?>
+	</style>
+	
+</head>
+<body>
+	
+
+<div class="container-fluid upperline">
+	<div class="row justify-content-end pr-5">	
+			
+
+	</div>
+</div>
+<div class="container">	
+	<nav class="navbar pt-3 navbar-expand-lg fixed-top navbar-fixed-top right">
+		<div class="container">
+		
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav mx-auto">
+			      <li class="nav-item active">
+			        <a class="nav-link px-3" href="index.php">HOME <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link px-3" href="shop.php">SHOP</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link px-3" href="seller_regi.php">BECOME A SELLER</a>
+			      </li> 
+			      
+			  </ul>
+			  <ul class="navbar-nav ml-auto">
+			    <form class="form-inline my-2 my-lg-0">
+			      <button class="btn btn-outline-success my-2 my-sm-0 button-search" type="submit">
+			      	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+					  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+					</svg>
+			      </button>
+			    </form>
+			      <li class="nav-item px-3 ">
+			        <a class="nav-link" href="cart.php">
+			          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+						  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+						</svg>
+			        </a>
+					<li class="nav-item px-3 ">
+			        <a class="nav-link" href="log_reg.php">
+			          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+						  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+						  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+						</svg>
+			        </a>
+			      </li>
+			    </ul>
+			  </div>
+		</div>
+	</nav>
---
+++ log_reg.php
@@ -0,0 +1,133 @@
+<?php 
+session_start();
+
+include_once('database.php');
+
+// this is for login script
+if(isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $encrypted_pass = password_hash($password, PASSWORD_BCRYPT);
+    
+    $verify_statement = $database->verify_customers($username, $password);
+    
+
+    if(!$verify_statement) {
+        echo "Failed!";
+    } else {
+        echo '<script> alert("Success!") </script';
+        $_SESSION['user'] = $verify_statement;
+		header("Location: user_home.php");
+    }   
+}
+?>
+
+
+
+<?php
+if(isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $encrypted_pass = password_hash($password, PASSWORD_BCRYPT);
+
+    $create_customer_statement = $database->create_customers($username, $email, $encrypted_pass);
+
+    if($create_customer_statement) {
+        echo '<script> alert("Success!") </script>';
+    } else {
+        echo '<script> alert("Failed!") </script>';
+    }
+}
+
+
+?>
+
+
+
+
+<?php
+ include_once 'header.php';
+?>
+
+
+    <style type="text/css">
+    <?php
+     include 'css/log_reg.css';
+    ?>
+   </style> 
+
+<!-- CAROUSEL -->
+<div id="carouselExampleCaptions" class="carousel slide h-100" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/banner/farmerhands.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption">
+        <h1>My Account</h1>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+</div>
+<hr>
+   
+<!-- END OF CAROUSEL -->
+
+
+<!-- LOGIN/REGI FORM -->
+
+<div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form">
+                    <h3>Login</h3>
+                    <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email or Username" value="" name="username" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password " value="" name="password" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" name="login" />
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd">Forget Password?</a>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6 regi-form">
+                    <h3>Register</h3>
+                    <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email " value="" name="email" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Username" value="" name="username" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password " value="" name="password" />
+                        </div>
+
+                        <div class="form-group">
+                            <p class="text_policy">Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.</p>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Register" />
+                            
+                        </div>
+                        <div class="form-group">
+                        <button type="button" class="btnSubmit" onclick="location.href='seller_regi.php';" />Become a Seller</button>
+                        </div>
+
+                    
+                    </form>
+                </div>
+            </div>
+        </div>
+</div>
+<!-- END OF LOGIN REGI FORM -->
+
+<?php
+include_once 'footer.php';
+?>
